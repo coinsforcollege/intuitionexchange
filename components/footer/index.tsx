4c8a2ce2b310c49108290b5972199fd77245e5db
samarmeena <sumarmeena@gmail.com>
2023-05-16 00:15:31 +0530

    refactor: footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,6 +4,15 @@ import Link from "next/link";
 
 import LogoImg from "../../public/logo.svg";
 import styles from "./footer.module.css";
+import { css } from "@emotion/css";
+import mq from "util/breakpoints";
+
+const centerFooter = css({
+  [mq.sm]: {
+    width: "fit-content",
+    margin: "auto",
+  }
+});
 
 export default function Footer(props: { fullWidth?: boolean }) {
   return (
@@ -15,87 +24,94 @@ export default function Footer(props: { fullWidth?: boolean }) {
       <div>
         <Row>
           <Col xs={12} sm={12} md={8}>
-            <Typography.Title level={4}>Compliance</Typography.Title>
-            <ul className={styles.footerList}>
-              <li>
-                <Link href="/privacy-policy" className="footer-link">
-                  Privacy Policy
-                </Link>
-              </li>
-              <li>
-                <Link href="/terms-of-use" className="footer-link">
-                  Terms of use
-                </Link>
-              </li>
-              <li>
-                <Link href="/anti-fraud-policy" className="footer-link">
-                  Anti Fraud Policy
-                </Link>
-              </li>
-              <li>
-                <Link href="/complaint-management" className="footer-link">
-                  Complaint Management
-                </Link>
-              </li>
-              <li>
-                <Link href="/bsa-policy" className="footer-link">
-                  BSA Policy
-                </Link>
-              </li>
-            </ul>
+            <div className={centerFooter}>
+              <Typography.Title level={4}>Compliance</Typography.Title>
+              <ul className={styles.footerList}>
+                <li>
+                  <Link href="/privacy-policy" className="footer-link">
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/terms-of-use" className="footer-link">
+                    Terms of use
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/anti-fraud-policy" className="footer-link">
+                    Anti Fraud Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/complaint-management" className="footer-link">
+                    Complaint Management
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/bsa-policy" className="footer-link">
+                    BSA Policy
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </Col>
           <Col xs={12} sm={12} md={8}>
-            <Typography.Title level={4}>User Services</Typography.Title>
-            <ul className={styles.footerList}>
-              <li>
-                <Link href="/wallet" className="footer-link">
-                  Wallet
-                </Link>
-              </li>
-              <li>
-                <Link href="/exchange" className="footer-link">
-                  Exchange
-                </Link>
-              </li>
-              <li>
-                <Link href="/p2p" className="footer-link">
-                  P2P
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="https://support.intuitionexchange.com"
-                  className="footer-link"
-                  target="_blank"
-                >
-                  Help Center
-                </Link>
-              </li>
-            </ul>
+            <div className={centerFooter}>
+
+              <Typography.Title level={4}>User Services</Typography.Title>
+              <ul className={styles.footerList}>
+                <li>
+                  <Link href="/wallet" className="footer-link">
+                    Wallet
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/exchange" className="footer-link">
+                    Exchange
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/p2p" className="footer-link">
+                    P2P
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://support.intuitionexchange.com"
+                    className="footer-link"
+                    target="_blank"
+                  >
+                    Help Center
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </Col>
           <Col xs={12} sm={12} md={8}>
-            <Typography.Title level={4}>About</Typography.Title>
-            <ul className={styles.footerList}>
-              <li>
-                <Link href="/" className="footer-link">
-                  About InTuition
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="https://www.primetrust.com/about"
-                  className="footer-link"
-                  target="_blank"
-                >
-                  Prime Trust
-                </Link>
-              </li>
-              <li>
-                <Link href="/" className="footer-link">
-                  Listing Enquiry
-                </Link>
-              </li>
-            </ul>
+            <div className={centerFooter}>
+              <Typography.Title level={4}>About</Typography.Title>
+              <ul className={styles.footerList}>
+                <li>
+                  <Link href="/" className="footer-link">
+                    About InTuition
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.primetrust.com/about"
+                    className="footer-link"
+                    target="_blank"
+                  >
+                    Prime Trust
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="footer-link">
+                    Listing Enquiry
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </Col>
         </Row>
         <Divider />
@@ -113,6 +129,6 @@ export default function Footer(props: { fullWidth?: boolean }) {
           </Typography.Text>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
